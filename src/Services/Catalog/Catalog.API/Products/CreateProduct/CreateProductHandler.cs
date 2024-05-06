337d25b2112e358de843da4e749caa3529a952c7
qal <lequyetanh@outlook.com>
2024-05-07 02:01:17 +0700

    create buildingBlocks

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,5 +1,18 @@
-﻿namespace Catalog.API.Products.CreateProduct;
+﻿using MediatR;
 
-public class CreateProductHandler
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Categories, string Description, string ImageFile, decimal Price)
+    : IRequest<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+
+internal class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
 {
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        // Business logic to create a product
+        throw new NotImplementedException();
+    }
 }
