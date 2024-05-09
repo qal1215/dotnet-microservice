8bbae3ee60d20a2a43a96f69f874fe7d5b5ec454
qal <lequyetanh@outlook.com>
2024-05-09 20:57:28 +0700

    Add custom exception and create getProductById api

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Catalog.API.Products.GetProductById;
+
+public record GetProductRequest(Guid Id);
+public record GetProductResponse(Product Product);
+
+public class GetProductByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByIdQuery(id));
+
+            var response = result.Adapt<GetProductResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetProductById")
+        .Produces<GetProductResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Gets a product by its ID")
+        .WithDescription("Gets a product by its ID from the catalog");
+    }
+}
