fb88e56af92f6370caf591caffb1aed695e4af47
qal <lequyetanh@outlook.com>
2024-05-11 14:52:42 +0700

    add delete and update product

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCatelogyHandler .cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace Catalog.API.Products.GetProductByCategory;
+
+public record GetProductByCategoryQuery(string Catelogy) : IQuery<GetProductByCategoryResult>;
+public record GetProductByCategoryResult(IEnumerable<Product> Products);
+
+internal class GetProductByCategoryQueryHandler
+    (IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> logger)
+    : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+{
+    public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductByCatelogyQueryHandler.Handle called with {@Query}", query);
+
+        var products = await session.Query<Product>()
+            .Where(p => p.Categories.Contains(query.Catelogy))
+            .ToListAsync(cancellationToken);
+
+        return new GetProductByCategoryResult(products);
+    }
+}
