1d18cbbaf52d0547db774c16bae5b7c9aa37d3cd
qal <lequyetanh@outlook.com>
2024-05-14 14:50:52 +0700

    add fuentValidator and build validationBehavior

---
+++ src/BuildingBlocks/BuildingBlocks/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,15 @@
+﻿using BuildingBlocks.CQRS;
+using MediatR;
+
+namespace BuildingBlocks.Behaviors;
+
+public class ValidationBehavior<TRequest, TResponse>
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : ICommand<TResponse>
+{
+    public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
+
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -2,10 +2,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddCarter();
+
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
+
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("dbcnt")!);
