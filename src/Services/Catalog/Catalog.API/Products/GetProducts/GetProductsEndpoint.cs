e28843df2e0a5861201dbc514d831b40089c7987
qal <lequyetanh@outlook.com>
2024-05-29 00:36:24 +0700

    Add discount service

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -1,4 +1,4 @@
-﻿namespace Catalog.API.Products.GetProduct;
+﻿namespace Catalog.API.Products.GetProducts;
 
 public record GetProductsRequest(int? PageNumber = 1, int? PageSize = 10);
 
--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,4 +1,4 @@
-﻿namespace Catalog.API.Products.GetProduct;
+﻿namespace Catalog.API.Products.GetProducts;
 
 public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10) : IQuery<GetProductsResult>;
 
