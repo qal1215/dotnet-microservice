1e6e88a5c97c2ce8a9c271a492119642a0d19ac7
qal <lequyetanh@outlook.com>
2024-05-29 18:20:01 +0700

    Done Section 11. Discount gRPC

---
+++ src/Services/Discount/Discount.Grpc/Data/DbExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data;
+
+public static class DbExtensions
+{
+    public static async Task<IApplicationBuilder> UseMigrationAsync(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        using var context = scope.ServiceProvider.GetRequiredService<DiscountContext>();
+        await context.Database.MigrateAsync();
+
+        return app;
+    }
+}
