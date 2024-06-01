f3da6f1799e4c63eab7e1e7ab461bf76d5b00d6a
qal <lequyetanh@outlook.com>
2024-06-02 01:00:17 +0700

    Section 13, Create structure code and register DI services

---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.API;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiServices
+        (this IServiceCollection services)
+    {
+
+        return services;
+    }
+}
