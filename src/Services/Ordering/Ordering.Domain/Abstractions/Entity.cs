0cf1e44e5485683562a3aa9df2ce1edd2de9f9b1
qal <lequyetanh@outlook.com>
2024-06-14 19:35:18 +0700

    add strong type id

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Entity.cs
@@ -0,0 +1,9 @@
+﻿namespace Ordering.Domain.Abstractions;
+public abstract class Entity<T> : IEntity<T>
+{
+    public T Id { get; set; }
+    public DateTime? CreateAt { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime? LastModifiedAt { get; set; }
+    public string? LastModifiedBy { get; set; }
+}
